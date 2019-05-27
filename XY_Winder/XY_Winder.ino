a19a3a7ba4a5ce1eba9de555a7063c77197d9e71
Jess <Jess@sulliwood.org>
2019-05-27 16:27:41 -0400

    init sketches

---
+++ XY_Winder/XY_Winder.ino
@@ -0,0 +1,70 @@
+/*
+ * An Arduino sketch to wind a coil with two bipolar stepper motors
+ * Currently tested with DRV8825
+ * 
+ * WIP by Jess Sullivan
+ * 
+ * TODO:  
+ * wind back and forth 
+ * auto length start/stop based on coil perimeter 
+ * standalone power connections
+ * start stop with SPST switch
+ */
+
+// wire width in mm
+const float wire = .1;
+// coil width in mm - for back and forth
+// const int width = 7;
+
+// connections
+const int X_dir = 2;
+const int X_step = 3;
+const int Y_dir = 4;
+const int Y_step = 5;
+
+// motor definitions 
+const int rev = 200;
+const int rate = 5;
+
+// winder guide pitch in mm
+const int pitch = 2;
+
+// winder guide calculations
+const float reduction = pitch / wire;
+const unsigned long ratio = rate * reduction;
+
+void setup()
+{
+  // setup outputs
+  pinMode(X_dir, OUTPUT);
+  pinMode(X_step, OUTPUT);
+  pinMode(Y_dir, OUTPUT);
+  pinMode(Y_step, OUTPUT);
+}
+
+// start timers 
+unsigned long X_time = millis();
+unsigned long Y_time = millis();
+
+void loop()
+{
+  // motor directions
+  digitalWrite(X_dir, LOW);
+  digitalWrite(Y_dir, LOW);
+  // pulse motors
+  {
+    digitalWrite(Y_step, LOW);
+    if (millis() - Y_time > ratio)
+    {
+      digitalWrite(Y_step, HIGH);
+      Y_time = millis();
+    }
+    
+    digitalWrite(X_step, LOW);
+    if (millis() - X_time > rate)
+    {
+      digitalWrite(X_step, HIGH);
+      X_time = millis();
+    }
+  }
+}
